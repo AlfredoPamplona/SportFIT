e6214ede0e4ff6bde003cf6511aa175461206067
AlfredoPamplona <alfredopamplona4@gmail.com>
2024-05-16 20:09:55 +0200

    Primer informe hecho y los 3 graficos hechos quiero que como titulo no muestre 0)

--- SportFIT/Controllers/EstadisticasController.cs
+++ SportFIT/Controllers/EstadisticasController.cs
@@ -1,12 +1,126 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
+using System.Data;
 
 namespace SportFIT.Controllers
 {
-    internal class EstadisticasController
+    public class EstadisticasController
     {
+        private readonly string connectionString;
+
+        public EstadisticasController(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public DataTable ObtenerReservasPorInstalacion(string pueblo)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = @"
+                                SELECT
+                                    i.nombre_instalacion AS Instalacion,
+                                    COUNT(r.id_reserva) AS Total_Reservas
+                                FROM
+                                    Instalacion i
+                                JOIN
+                                    Reserva r ON i.id_instalacion = r.id_instalacion
+                                JOIN Pueblo P on i.id_pueblo = P.id_pueblo
+                                WHERE p.nombre_pueblo = @NombrePueblo
+                                GROUP BY
+                                    i.nombre_instalacion";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@NombrePueblo", pueblo);
+                    connection.Open();
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+
+            return dataTable;
+        }
+
+        public DataTable ObtenerPorcentajesReservas(string pueblo)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = @"
+                    SELECT
+                        u.nombre_usuario AS Usuario,
+                        COUNT(*) AS Total_Reservas,
+                        ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (), 2) AS Porcentaje_Reservas
+                    FROM
+                        Usuario u
+                    JOIN
+                        Reserva r ON u.id_usuario = r.id_usuario
+                    JOIN
+                        Instalacion i ON r.id_instalacion = i.id_instalacion
+                    JOIN
+                        Pueblo P ON i.id_pueblo = P.id_pueblo
+                    WHERE
+                        p.nombre_pueblo = @NombrePueblo
+                    GROUP BY
+                        u.nombre_usuario";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@NombrePueblo", pueblo);
+                    connection.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+
+            return dataTable;
+        }
+
+        public DataTable TendenciaReservasMes(string pueblo)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = @"
+                    SELECT
+                        YEAR(r.fecha_reserva) AS Anio,
+                        DATENAME(MONTH, r.fecha_reserva) AS Mes,
+                        COUNT(*) AS Total_Reservas
+                    FROM
+                        Reserva r
+                    JOIN
+                        Instalacion i ON r.id_instalacion = i.id_instalacion
+                    JOIN Pueblo P on i.id_pueblo = P.id_pueblo
+                    WHERE p.nombre_pueblo = @NombrePueblo
+                    GROUP BY
+                        YEAR(r.fecha_reserva),
+                        DATENAME(MONTH, r.fecha_reserva)
+                    ORDER BY
+                        Anio,
+                        mes";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@NombrePueblo", pueblo);
+                    connection.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+
+            return dataTable;
+        }
     }
 }
--- SportFIT/Views/UserControls/EstadisticasControl.xaml.cs
+++ SportFIT/Views/UserControls/EstadisticasControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,10 @@ using System.Windows.Shapes;
 using LiveCharts;
 using LiveCharts.Wpf;
 using SportFIT.Controllers;
+using Stimulsoft.Report.Viewer;
+using Stimulsoft.Report;
+using Stimulsoft.Report.WpfDesign;
+
 
 namespace SportFIT.Views.UserControls
 {
@@ -23,6 +28,7 @@ namespace SportFIT.Views.UserControls
     /// </summary>
     public partial class EstadisticasControl : UserControl
     {
+        private readonly EstadisticasController estadisticasController;
         private readonly PueblosController pueblosController;
         private string usuario;
         private string password;
@@ -33,6 +39,8 @@ namespace SportFIT.Views.UserControls
             this.usuario = usuario;
             this.password = password;
 
+            estadisticasController = new EstadisticasController(connectionString);
+
             // Crear una instancia del controlador de pueblos con la cadena de conexión adecuada
             pueblosController = new PueblosController(connectionString);
 
@@ -42,49 +50,49 @@ namespace SportFIT.Views.UserControls
             //Seleccionar el primer item por defecto
             comboBoxPueblos.SelectedIndex = 0;
 
-            // Cargar datos de prueba en el gráfico de barras
-            SeriesCollection barSeries = new SeriesCollection
-            {
-                new ColumnSeries
-                {
-                    Title = "Reservas Por Instalación",
-                    Values = new ChartValues<int> { 10, 20, 30, 40 }
-                }
-            };
-            BarChart.Series = barSeries;
-            // Cargar datos de prueba en el gráfico circular
-            SeriesCollection pieSeries = new SeriesCollection
-            {
-                new PieSeries
-                {
-                    Title = "Media duración reservas Usuario",
-                    Values = new ChartValues<double> { 30 },
-                    DataLabels = true
-                },
-                new PieSeries
-                {
-                    Title = "Media duración reservas Administrador",
-                    Values = new ChartValues<double> { 50 },
-                    DataLabels = true
-                },
-                new PieSeries
-                {
-                    Title = "Media duración reservas Monitor",
-                    Values = new ChartValues<double> { 20 },
-                    DataLabels = true
-                }
-            };
-            PieChart.Series = pieSeries;
-            // Cargar datos de prueba en el gráfico de líneas
-            SeriesCollection lineSeries = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Title = "Tendencia mensual reservas",
-                    Values = new ChartValues<double> { 10, 20, 15, 40, 80, 65, 95 } // Valores de ejemplo para la serie de línea
-                }
-            };
-            LineChart.Series = lineSeries;
+            //// Cargar datos de prueba en el gráfico de barras
+            //SeriesCollection barSeries = new SeriesCollection
+            //{
+            //    new ColumnSeries
+            //    {
+            //        Title = "Reservas Por Instalación",
+            //        Values = new ChartValues<int> { 10, 20, 30, 40 }
+            //    }
+            //};
+            //BarChart.Series = barSeries;
+            //// Cargar datos de prueba en el gráfico circular
+            //SeriesCollection pieSeries = new SeriesCollection
+            //{
+            //    new PieSeries
+            //    {
+            //        Title = "Media duración reservas Usuario",
+            //        Values = new ChartValues<double> { 30 },
+            //        DataLabels = true
+            //    },
+            //    new PieSeries
+            //    {
+            //        Title = "Media duración reservas Administrador",
+            //        Values = new ChartValues<double> { 50 },
+            //        DataLabels = true
+            //    },
+            //    new PieSeries
+            //    {
+            //        Title = "Media duración reservas Monitor",
+            //        Values = new ChartValues<double> { 20 },
+            //        DataLabels = true
+            //    }
+            //};
+            //PieChart.Series = pieSeries;
+            //// Cargar datos de prueba en el gráfico de líneas
+            //SeriesCollection lineSeries = new SeriesCollection
+            //{
+            //    new LineSeries
+            //    {
+            //        Title = "Tendencia mensual reservas",
+            //        Values = new ChartValues<double> { 10, 20, 15, 40, 80, 65, 95 } // Valores de ejemplo para la serie de línea
+            //    }
+            //};
+            //LineChart.Series = lineSeries;
         }
         private void CargarNombresPueblos()
         {
@@ -102,10 +110,105 @@ namespace SportFIT.Views.UserControls
         {
             if (comboBoxPueblos.SelectedItem != null)
             {
-                // Obtener el nombre del pueblo seleccionado
-                string selectedPueblo = comboBoxPueblos.SelectedItem.ToString();
-
+                CargarDatosGraficoBarras(); // Llamar al método para cargar los datos del gráfico de barras
+                CargarDatosGraficoRedondo(); // Llamar al método para cargar los datos del gráfico circular
+                CargarDatosGraficoLineas(); // Llamar al método para cargar los datos del gráfico de lineas
             }
+        }
+
+        private void CargarDatosGraficoBarras()
+        {
+            string selectedPueblo = comboBoxPueblos.SelectedItem.ToString();
+            DataTable reservasPorInstalacion = estadisticasController.ObtenerReservasPorInstalacion(selectedPueblo);
+
+            SeriesCollection barSeries = new SeriesCollection();
+
+            foreach (DataRow row in reservasPorInstalacion.Rows)
+            {
+                string nombreInstalacion = row["Instalacion"].ToString();
+                int totalReservas = Convert.ToInt32(row["Total_Reservas"]);
+
+                ColumnSeries series = new ColumnSeries
+                {
+
+                    Title = "Reservas por Instalación: " + nombreInstalacion,
+                    Values = new ChartValues<int> { totalReservas }
+                };
+
+                barSeries.Add(series);
+            }
+
+            BarChart.Series = barSeries;
+        }
+
+        private void CargarDatosGraficoRedondo()
+        {
+            string selectedPueblo = comboBoxPueblos.SelectedItem.ToString();
+            DataTable porcentajesReservas = estadisticasController.ObtenerPorcentajesReservas(selectedPueblo);
+
+            SeriesCollection pieSeries = new SeriesCollection();
+
+            foreach (DataRow row in porcentajesReservas.Rows)
+            {
+                string nombreUsuario = row["Usuario"].ToString();
+                double porcentajeReservas = Convert.ToDouble(row["Porcentaje_Reservas"]);
+
+                PieSeries series = new PieSeries
+                {
+                    Title = nombreUsuario,
+                    Values = new ChartValues<double> { porcentajeReservas },
+                    DataLabels = true
+                };
+
+                pieSeries.Add(series);
+            }
+
+            PieChart.Series = pieSeries;
+        }
+        private void CargarDatosGraficoLineas()
+        {
+            try
+            {
+                string selectedPueblo = comboBoxPueblos.SelectedItem.ToString();
+                DataTable tendenciaReservas = estadisticasController.TendenciaReservasMes(selectedPueblo);
+
+                LineChart.Series.Clear();
+
+                LineSeries series = new LineSeries
+                {
+                    Title = "Tendencia mensual de reservas",
+                    Values = new ChartValues<int>()
+
+                };
+
+                foreach (DataRow row in tendenciaReservas.Rows)
+                {
+                    string mes = row["Mes"].ToString();
+                    int totalReservas = Convert.ToInt32(row["Total_Reservas"]);
+
+                    series.Values.Add(totalReservas);
+                }
+
+                LineChart.Series.Add(series);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los datos para el gráfico de líneas: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private void InformeAgrupacionInstalaciones_Click(object sender, RoutedEventArgs e)
+        {
+            // Crear una instancia del informe diseñado en Stimulsoft
+            StiReport report = new StiReport();
+
+            // Cargar el informe desde el archivo
+            report.Load("..\\..\\Reports\\InformeAgrupacionReservas.mrt");
+            report["PuebloId"] = pueblosController.ObtenerPuebloSelected(comboBoxPueblos.Text);
+            report.Compile();
+            
+            report.Show();
         }
     }
 }
